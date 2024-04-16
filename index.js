b548b2592f3926560916d00d8729325fa743e1c0
brnomosa <omosabria99@gmail.com>
2024-04-16 12:37:31 +0300

    find

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+    const win = record.find(({result}) => result === "W")
+if (win){
+    return win.year
+}else {
+    return undefined
+}
+}
